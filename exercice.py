b25729614bc0b922b70b651677e303e3df038dbc
totyrola <astir.tadrous@polymtl.ca>
2020-09-09 19:48:46 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -9,8 +9,8 @@ def square_root(number: int) -> float:
 
 def square(number: int) -> int:
     # TODO completer la fonction
-    return math.pow(number, 2)
-    
+    return ma.pow(number, 2)
+
 
 
 def main() -> None:
